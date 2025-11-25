ad5776b2a9ac38ed4987370d3bbd3c1bb7868f1b
Colvin-Y <ykwhrimfaxi@gmail.com>
2025-11-25 22:30:47 +0800

    调整落款
    Signed-off-by: Colvin-Y <ykwhrimfaxi@gmail.com>

--- main.py
+++ main.py
@@ -2461,8 +2461,8 @@ def render_html_content(
             
             <div class="footer">
                 <div class="footer-content">
-                    由 <span class="project-name">TrendRadar</span> 生成 · 
-                    <a href="https://github.com/sansan0/TrendRadar" target="_blank" class="footer-link">
+                    由 <span class="project-name">Colvin-Y/TrendRadar</span> 生成 · 
+                    <a href="https://github.com/Colvin-Y/TrendRadar" target="_blank" class="footer-link">
                         GitHub 开源项目
                     </a>"""
 
